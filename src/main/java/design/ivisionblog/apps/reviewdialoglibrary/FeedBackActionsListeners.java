29d8eee5ebcc2d1a5f5b70134e21c19983a3b16d
shivasurya <s.shivasurya@gmail.com>
2018-01-13 18:50:58 +0530

    initial library feedback API added

---
+++ src/main/java/design/ivisionblog/apps/reviewdialoglibrary/FeedBackActionsListeners.java
@@ -0,0 +1,11 @@
+package design.ivisionblog.apps.reviewdialoglibrary;
+
+/**
+ * Created by Shivasurya S on 13/01/18.
+ */
+public interface FeedBackActionsListeners
+{
+    public void onSuccess();
+    public void onFailure();
+    public void onAmbiguity();
+}
